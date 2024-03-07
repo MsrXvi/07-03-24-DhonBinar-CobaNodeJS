41b4b3c9936015420dc5f268b319cbed7205d950
Muhammad Syahri Romadhoni <121951868+MsrXvi@users.noreply.github.com>
2024-03-07 22:07:53 +0700

    coba nodejs

--- server/index.js
+++ server/index.js
@@ -1,1 +1,24 @@
-console.log("Implement servermu disini yak 😝!");
+// core package => built in node js
+const http = require ("http");
+const fs = require ("fs");
+const path = require("path");
+
+const url = require("url");
+
+const PUBLIC_DIR = path.join(__dirname, "../public");
+const PORT = 8000;
+
+const server = (req, res) => {
+
+    if (req.url ==='/') {
+        res.end('ini default server')
+    }
+    else if (req.url === '/search') {
+        res.end("ini halaman search")
+    } else {
+        res.end("gak ada...")
+    }
+
+};
+http.createServer(server).listen(PORT);
+console.log(`server is running ... PORT : localhost:${PORT}`);
